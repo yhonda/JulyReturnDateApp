11c53587bf19d70e145c03c69bc2aeeaa5e2e7d4
stv-ekushida <e.kushida@st-ventures.jp>
2018-07-05 14:39:51 +0900

    Initial Commit

---
+++ JulyReturnDateApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JulyReturnDateApp
+//
+//  Created by Eiji Kushida on 2018/07/05.
+//  Copyright © 2018年 Eiji Kushida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
